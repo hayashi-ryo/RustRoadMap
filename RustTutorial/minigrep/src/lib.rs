54123123d8028178ab3c9d4bcd0a0cbbf263b76c
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2025-01-01 18:14:43 +0900

    [feat]: iter and closure

--- RustTutorial/minigrep/src/lib.rs
+++ RustTutorial/minigrep/src/lib.rs
@@ -9,15 +9,28 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let case_sensitibe = env::var("CASE_INSENSITIVE").is_err();
-        Ok(Config { query, filename })
+        let query = match args.next() {
+            Some(arg) => arg,
+            // クエリ文字列を取得しませんでした
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            // ファイル名を取得しませんでした
+            None => return Err("Didn't get a file name"),
+        };
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
@@ -31,14 +44,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
