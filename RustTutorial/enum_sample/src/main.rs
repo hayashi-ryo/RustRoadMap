db55c53323909fa76148a8b512dd625f739fed42
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2025-01-02 23:39:22 +0900

    [feat]: 並行性

--- RustTutorial/enum_sample/src/main.rs
+++ RustTutorial/enum_sample/src/main.rs
@@ -6,6 +6,6 @@ enum IPAddrKind {
 
 fn main() {
     // 以下のように具体的な構造体を定義しなくても、enumのそれぞれの要素に対して具体的な型を保持することができる。
-    let home = IPAddr::V4(String::from("127.0.0.1"))
-    let home = IPAddr::V6(String::from("::1"))
+    let home = IPAddr::V4(String::from("127.0.0.1"));
+    let home = IPAddr::V6(String::from("::1"));
 }
